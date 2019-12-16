2ec7efd12924012909e5c8c19abecc6c08428cbe
Alex Gallant <agallant1201@gmail.com>
2019-12-16 07:56:29 -0700

    Create author show spec

---
+++ spec/features/author_show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Visiting author show page" do
+  it "I see an author's information and their books information" do
+    book_1 = Book.create(title: "New Book", publication_year: "1988", pages: "250")
+    author_1 = Author.create(name: "Billy Bob")
+    author_1.books << book_1
+
+    visit "/authors/#{author_1.id}"
+
+    expect(page).to have_content(author_1.name)
+    expect(page).to have_content(book_1.title)
+  end
+
+  it "can show the average number of pages of each author's books" do
+    book_1 = Book.create(title: "First Book", publication_year: "1988", pages: "200")
+    book_2 = Book.create(title: "Second Book", publication_year: "1988", pages: "400")
+    author_1 = Author.create(name: "Billy Bob")
+    author_1.books << book_1
+    author_1.books << book_2
+
+    expect(page).to have_content("300")
+  end
+end
